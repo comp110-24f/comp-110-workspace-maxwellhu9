a159356aff5a49c1c5cdd74de3ce6eb57ad79bdb
maxwellhu9 <maxhu@ad.unc.edu>
2024-09-16 22:40:15 -0400

    Finished

---
+++ lessons/functions.py
@@ -0,0 +1,11 @@
+# from random import randint
+
+# print(randint(3, 1))
+
+
+def sum(num1: int, num2: int) -> int:
+    """return the sum of num1 and num2"""
+    return num1 + num2
+
+
+print(sum(num1=2, num2=13))
